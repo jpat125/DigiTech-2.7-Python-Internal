cf16c1eb137aa201107778b788255379a08a9096
Jaiden Patel <jaidenpat380@gmail.com>
2022-06-02 19:36:11 +1200

    Added extra features
    Added continuation feature.

--- Initalquestion.py
+++ Initalquestion.py
@@ -1,7 +1,20 @@
 #Create def to ask user inital question (issue with device or website?)
 
 def initalquestion():
-    iq=input("""Is your issue regarding a:
-    1 - Device
-    2 Website
-    """)
+    while True:
+        iq=input("""Is your issue regarding a:
+        1 - Device
+        2 Website
+        """)
+        if iq == 1:
+            print ("You have selected: Device ")
+        elif iq == 2:
+            print ("You have selected: Website ") 
+        else:
+            print ("Unrecognised input!")
+        contq = input("Do you wish to continue? (y or n").strip().lower()
+        if contq == "y":
+            print ("Continuing")
+            break
+        else:
+            ()

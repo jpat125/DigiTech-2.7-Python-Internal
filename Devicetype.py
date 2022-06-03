9f7510abf06255dc650f47d0f49195c7b78fe71f
Jaiden Patel <jaidenpat380@gmail.com>
2022-06-03 14:41:24 +1200

    Added in Continue section
    Added in Continue section as well as extra features and formatting.

--- Devicetype.py
+++ Devicetype.py
@@ -5,35 +5,38 @@ from glob import glob
 def devicetype():
     while True:
         global devty
-        devty=int(input("""Please select the type of device you are having an issue with:
+        devty=(input("""Please select the type of device you are having an issue with:
 
-    1 - Laptop
-    2 - Ipad (Apple only)
-    3 - Tablet
-    4 - Apple Mac
-    """))  
+1 - Laptop
+2 - Ipad (Apple only)
+3 - Tablet
+4 - Apple Mac
+"""))
 
         if devty == "1":
-            print ("laptop")
+            print ("You have selected: Laptop")
         elif devty == "2":
-            print ("Ipad")
+            print ("You have selected: Ipad")
         elif devty == "3":
-            print ("Tablet")
+            print ("You have selected: Tablet")
         elif devty == "4":
-            print ("Apple Mac")
+            print ("You have selected: Apple Mac")
         else:
             print ("Unrecognised input")
-            contq = input("Do you wish to continue? (y or n) ").strip().lower()
+
+        print('\n')
+        contq = input("Do you wish to continue? (y or n) ").strip().lower()
+        print ('\n')
+        if contq == "y" or contq == "yes":
+            print ("Continuing")
             print ('\n')
-            if contq == "y" or contq == "yes":
-                print ("Continuing")
-                print ('\n')
-                break
-            elif contq == "n" or contq == "no":
-                print ("Stopping")
-                break
-            else:
-                print("Unrecognised input")
-                print ('\n')
+            break
+        elif contq == "n" or contq == "no":
+            print ("Stopping")
+            print ('\n')
+            break
+        else:
+            print("Unrecognised input")   
+            print ('\n')
 
 devicetype()

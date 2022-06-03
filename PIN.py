a10fc8803aaa558dd9eb45fb88ffc4ed9b393300
Jaiden Patel <jaidenpat380@gmail.com>
2022-06-03 13:42:41 +1200

    Added in While loop to return error if PIN wrong
    Added While True loop to throw back error if wrong PIN entered.

--- PIN.py
+++ PIN.py
@@ -1,8 +1,14 @@
 #Creating def for PIN access 
 
 def PIN_access():
-    userpin=int(input("Enter PIN: "))
-    if userpin == 1234:
-        print ("access granted")
+    while True:
+        userpin=int(input("Enter PIN: "))
+        if userpin == 1234:
+            print ("access granted")
+            print('\n')
+            break
+        else:
+            print ("Please try again:")
+            print ('\n')
 
 PIN_access()

cd7f11d4f385e97aefbd6b6a4532e447f273ea45
Jaiden Patel <jaidenpat380@gmail.com>
2022-06-08 11:48:26 +1200

    Update DeviceOS.py

--- DeviceOS.py
+++ DeviceOS.py
@@ -3,16 +3,20 @@ def deviceos():
     global deviceosv
     
     while True:
-        deviceosv=(input("What is the Operating System of the device? eg. Windows 10 or IpadOS (if unknown, type 'uknown') ").strip().lower())
+        deviceosv=(input("What is the Operating System of the device? eg. Windows 10, IpadOS, macOS (if unknown, type 'uknown') ").strip().lower())
         if deviceosv == "windows 10":
             print ("Windows 10")
         elif deviceosv =="windows 11":
+           print ("Windows 11")
         elif deviceosv == "ipados":
             print ("IpadOS")
         elif deviceosv == "ios":
             print ("IOS")
+        elif deviceos == "macos":
+            print ("macOS")
         else:
             print ("That doesn't seem to be a supported Operating System, perphaps you have mispelt?")
+            
         contq = input("Do you wish to continue? (y or n) ").strip().lower()
         print ('\n')
         if contq == "y" or contq == "yes":

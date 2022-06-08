6c2e77dd42b9f0f3cf339e0716050f7c0912d9d6
Jaiden Patel <jaidenpat380@gmail.com>
2022-06-08 12:13:13 +1200

    Create DeviceQ1.py

---
+++ DeviceQ1.py
@@ -0,0 +1,37 @@
+
+def devq1():
+    
+    while True:
+        global devq1v
+        devq1v = (int(input("""Please select one of the options below:
+1 - Display issue (including projecting)
+2 - Wi-Fi issues
+3 - File issues
+4 - Audio issues """).strip().lower()))
+        if devq1v == 1:
+            print("display issues")#leads to display issues question
+        elif devq1v == 2:
+            print ("wifi issues")#leads to wifi issues question
+        elif devq1v == 3:
+            print ("file issues")#leads to file issues issues question
+        elif devq1v == 4:
+            print ("audio issues")#leads to audio issues question
+        else:
+            print ("Unrecognised input")
+        
+        print ('\n')
+        contq = input("Do you wish to continue? (y or n) ").strip().lower()
+        print ('\n')
+        if contq == "y" or contq == "yes":
+            print ("Continuing")
+            print ('\n')
+            break
+        elif contq == "n" or contq == "no":
+                    print ("Stopping")
+                    print ('\n')
+                            
+        else:
+            print("Unrecognised input")  
+            print ('\n')
+
+devq1()

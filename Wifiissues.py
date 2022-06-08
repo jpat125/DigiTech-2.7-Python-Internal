8a5c952b062b992cd7045b6d0635a65a94002815
Jaiden Patel <jaidenpat380@gmail.com>
2022-06-09 10:02:23 +1200

    Update Wifiissues.py

--- Wifiissues.py
+++ Wifiissues.py
@@ -4,6 +4,7 @@ def wifissue():
 1 - Connecting
 2 - Remaining connected
 3 - Other
+
 """)))
         if wifiissuev == 1:
             print ("connecting issues")
@@ -20,7 +21,7 @@ def wifissue():
         print ('\n')
         contq = input("Do you wish to continue? (y or n) ").strip().lower()
         print ('\n')
-        if contq == "y" or contq == "yes":
+        if (contq == "y" or contq == "yes") and (wifiissuev == 1 or wifiissuev ==  2 or wifiissuev ==  3):
             print ("Continuing")
             print ('\n')
             break

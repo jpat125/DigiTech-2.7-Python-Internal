22e494e091c8b1f2ac71c1946bd9c10a9ba73c40
Jaiden Patel <jaidenpat380@gmail.com>
2022-06-09 09:30:32 +1200

    Update Wifiissues.py
    Added continuation feature.

--- Wifiissues.py
+++ Wifiissues.py
@@ -1,14 +1,31 @@
 def wifissue():
-    wifiissuev=(int(input("""I am having issues with:
- 1 - Connecting
- 2 - Remaining connected
- 3 - Other
- """)))
-    if wifiissuev == 1:
-        print ("connecting issues")
-    elif wifiissuev == 2:
-        print("remaining connected issue")
-    elif wifiissuev == 3:
-        print ("Other")
+    while True:
+        wifiissuev=(int(input("""I am having issues with:
+1 - Connecting
+2 - Remaining connected
+3 - Other
+""")))
+        if wifiissuev == 1:
+            print ("connecting issues")
+            
+        elif wifiissuev == 2:
+            print("remaining connected issue")
+            
+        elif wifiissuev == 3:
+            print ("Other")
+            
+        print ('\n')
+        contq = input("Do you wish to continue? (y or n) ").strip().lower()
+        print ('\n')
+        if contq == "y" or contq == "yes":
+            print ("Continuing")
+            print ('\n')
+            break
+        elif contq == "n" or contq == "no":
+            print ("Stopping")
+            print ('\n')                
+        else:
+            print("Unrecognised input")  
+            print ('\n')
 
 wifissue()

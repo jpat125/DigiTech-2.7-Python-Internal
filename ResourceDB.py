60b849533f43cb459e19bbc650a759804ace04af
Jaiden Patel <jaidenpat380@gmail.com>
2022-06-09 13:25:59 +1200

    Create ResourceDB.py

---
+++ ResourceDB.py
@@ -0,0 +1,30 @@
+wificonnecting = {"win10":"link",
+"win11":"link",
+"macOS":"link",
+"iPadOS":"link"}
+
+wifiremain = {"win10":"link",
+"win11":"link",
+"macOS":"link",
+"iPadOS":"link"}
+
+displayappletv = {"win10":"link",
+"win11":"link",
+"macOS":"link",
+"iPadOS":"link"}
+
+
+displaymirroring = {"win10":"link",
+"win11":"link",
+"macOS":"link",
+"iPadOS":"link"}
+
+displaymonitor = {"win10":"link",
+"win11":"link",
+"macOS":"link",
+"iPadOS":"link"}
+
+file = {"win10":"link",
+"win11":"link",
+"macOS":"link",
+"ipadOS":"link"}

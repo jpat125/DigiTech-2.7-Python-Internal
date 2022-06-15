82d81453267b36b12723fb8e0269d94d6a759d2c
Jaiden Patel <jaidenpat380@gmail.com>
2022-06-16 09:29:08 +1200

    Create Errorcodeissue.py

---
+++ Errorcodeissue.py
@@ -0,0 +1,9 @@
+import webbrowser
+import time
+
+def googlesearch():
+    taburl=("https://www.google.com/search?q=")
+    querey=(input("Input error code: "))
+    search=(taburl+querey+deviceosv)
+    webbrowser.open(search)
+    time.sleep (1.5)

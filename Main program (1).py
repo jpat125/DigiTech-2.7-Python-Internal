a97f7b9e1058cec3f1e62bcececf14438d2130b8
Jaiden Patel <jaidenpat380@gmail.com>
2022-06-16 10:08:36 +1200

    Update Main program (1).py

--- Main program (1).py
+++ Main program (1).py
@@ -17,51 +17,6 @@ def PIN_access():
             print ("Please try again:")
             print ('\n')
 
-
-#def asks user for the type of device that is having the issue
-
-
-def devicetype():
-    while True:
-        global devty
-        devty=(int(input("""Please select the type of device you are having an issue with:
-
-1 - Laptop
-2 - Ipad (Apple only)
-3 - Tablet
-4 - Apple Mac
-5 - Other
-
-""")))
-
-        if devty == 1:
-            print ("You have selected: Laptop")
-        elif devty == 2:
-            print ("You have selected: Ipad")
-        elif devty == 3:
-            print ("You have selected: Tablet")
-        elif devty == 4:
-            print ("You have selected: Apple Mac")
-        elif devty == 5:
-            print ("You have selected: Other")
-        else:
-            print ("Unrecognised input")
-
-        print('\n')
-        contq = input("Do you wish to continue? (y or n) ").strip().lower()
-        print ('\n')
-        if (contq == "y" or contq == "yes") and (devty == 1 or  devty == 2 or  devty == 3 or  devty == 4 or  devty == 5):   
-            print ("Continuing")
-            print ('\n')
-            break
-        elif contq == "n" or contq == "no":
-            print ("Stopping")
-            print ('\n')
-            
-        else:
-            print("Unrecognised input")   
-            print ('\n')
-
 #Create def to ask user inital question (issue with device or website?)
 def initalquestion():
     while True:
@@ -132,15 +87,60 @@ def whichweb():
             print("Unrecognised input")  
             print ('\n')
 
+
+#def asks user for the type of device that is having the issue
+
+def devicetype():
+    while True:
+        global devty
+        devty=(int(input("""Please select the type of device you are having an issue with:
+
+1 - Laptop
+2 - Ipad (Apple only)
+3 - Tablet
+4 - Apple Mac
+5 - Other
+
+""")))
+
+        if devty == 1:
+            print ("You have selected: Laptop")
+        elif devty == 2:
+            print ("You have selected: Ipad")
+        elif devty == 3:
+            print ("You have selected: Tablet")
+        elif devty == 4:
+            print ("You have selected: Apple Mac")
+        elif devty == 5:
+            print ("You have selected: Other")
+        else:
+            print ("Unrecognised input")
+
+        print('\n')
+        contq = input("Do you wish to continue? (y or n) ").strip().lower()
+        print ('\n')
+        if (contq == "y" or contq == "yes") and (devty == 1 or  devty == 2 or  devty == 3 or  devty == 4 or  devty == 5):   
+            print ("Continuing")
+            print ('\n')
+            break
+        
+        elif contq == "n" or contq == "no":
+            print ("Stopping")
+            print ('\n')
+            
+        else:
+            print("Unrecognised input")   
+            print ('\n')
+
 #devOS
 def deviceos():
     global deviceosv
     
     while True:
         deviceosv=(input("What is the Operating System of the device? eg. Windows 10, IpadOS, macOS (if unknown, type 'uknown') ").strip().lower())
-        if deviceosv == "windows 10":
+        if deviceosv == "windows10":
             print ("Windows 10")
-        elif deviceosv =="windows 11":
+        elif deviceosv =="windows11":
            print ("Windows 11")
         elif deviceosv == "ipados":
             print ("IpadOS")
@@ -168,7 +168,6 @@ def deviceos():
         else:
             print("Unrecognised input")  
             print ('\n')
-
 
 #display issue
 def displayissue():
@@ -207,7 +206,6 @@ def displayissue():
         else:
             print("Unrecognised input")  
             print ('\n')
-
 #audio issue
 def audioissue():
     while True:
@@ -242,7 +240,6 @@ def audioissue():
         else:
             print("Unrecognised input")  
             print ('\n')
-
 #wifi issue
 def wifissue():
     while True:
@@ -318,6 +315,13 @@ def googlesearch():
     search=(taburl+querey)
     webbrowser.open(search)
     time.sleep (1.5)
+#error code
+def errorcodesearch():
+    taburl=("https://www.google.com/search?q=")
+    errorcode=(input("Input error code: "))
+    search=(taburl+errorcode+deviceosv)
+    webbrowser.open(search)
+    time.sleep (1.5)
 
 
 #deviceQ1
@@ -343,9 +347,9 @@ def devq1():
         elif devq1v == 4:
             print (audioissue())#leads to audio issues question
         elif devq1v == 5:
-            print ("Error code")#leads to google search
+            print (errorcodesearch())#leads to google search
         elif devq1v == 6:
-            print ("other")#lead to google search
+            print (googlesearch())#lead to google search
         else:
             print ("Unrecognised input")
         

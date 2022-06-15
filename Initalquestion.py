126014f018421ff7a53a9bcab9296f815bf90f5d
Jaiden Patel <jaidenpat380@gmail.com>
2022-06-15 12:07:11 +1200

    Update Initalquestion.py

--- Initalquestion.py
+++ Initalquestion.py
@@ -1,32 +1,32 @@
 #Create def to ask user inital question (issue with device or website?)
-
 def initalquestion():
     while True:
-        iq=input("""Is your issue regarding a:
+        iq=(int(input("""Is your issue regarding a:
 
 1 - Device
 2 Website
 
-""")
+""")))
         print ('\n')
-        if iq == "1":
+        if iq == 1:
             print ("You have selected: Device ")
-        elif iq == "2":
+        elif iq == 2:
             print ("You have selected: Website ") 
         else:
             print ("Unrecognised input!")
+        print ('\n')
         contq = input("Do you wish to continue? (y or n) ").strip().lower()
         print ('\n')
-        if (contq == "y" or contq == "yes") and (iq == 1 or iq == 2): 
+        if (contq == "y" or contq == "yes") and (iq == 1 or iq == 2 or iq == 3 or iq == 4):    
             print ("Continuing")
             print ('\n')
             break
         elif contq == "n" or contq == "no":
-            print ("Stopping")
-            break
+                    print ("Stopping")
+                    print ('\n')
+                            
         else:
-            print("Unrecognised input")
+            print("Unrecognised input")  
             print ('\n')
-        
-        
+
 initalquestion()
